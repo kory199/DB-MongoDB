0d4962f700f074cf01f7f8fa96718a3aa68c9234
Lucid-Kim <97496901+kory199@users.noreply.github.com>
2023-01-06 14:56:14 +0900

    photon
    Creat Room
    Creat Matching System

--- Assets/Script/PhotonLauncher.cs
+++ Assets/Script/PhotonLauncher.cs
@@ -3,30 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
 
 public class PhotonLauncher : MonoBehaviourPunCallbacks
 {
-    [SerializeField] byte maxPlayer = 8;
+    byte maxPlayer = 2;
 
-    RoomOptions room = new RoomOptions();
+    [SerializeField] TextMeshProUGUI serverState = null;
+    [SerializeField] TextMeshProUGUI playerCount = null;
+    [SerializeField] TextMeshProUGUI roomName = null;
+    [SerializeField] TextMeshProUGUI buttonState = null;
+
+    [SerializeField] Button joinRoomButton = null;
+    [SerializeField] Button leaveRoomButton = null;
+
+    RoomOptions roomOptions = new RoomOptions();
 
     private void Awake()
     {
         // 룸 내의 로드된 레벨 동기화
         PhotonNetwork.AutomaticallySyncScene = true;
+
+        // 룸 생성 옵션 : MaxPlayer
+        roomOptions.MaxPlayers = maxPlayer;
+
+        // Object 초기화
+        Init();
     }
 
     private void Start()
     {
+        // 서버 연결
         SetPhotonNetWork();
+
+        // 서버 상태 출력
+        StatusServer();
         Debug.Log(PhotonNetwork.ServerAddress);
+
+        // PhotonNetwork.CreateRoom(null, roomOptions);
     }
 
     private void Update()
     {
-        //StatusServer();
+
     }
 
+    // Server Connect
     public void SetPhotonNetWork()
     {
         // PhotonServer 연결
@@ -35,7 +58,7 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsConnected)
         {
             Debug.Log("서버 연결 완료");
-            room.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable();
+            buttonState.text = "Conneced Server";
         }
 
         else
@@ -49,14 +72,16 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnMaster");
-        //PhotonNetwork.JoinRandomRoom();
-        //PhotonNetwork.JoinOrCreateRoom("test", new RoomOptions { MaxPlayers = maxPlayer }, null);
-        PhotonNetwork.JoinRandomOrCreateRoom(null, maxPlayer);
+
+        buttonState.text = "Join Room";
+
+        joinRoomButton.enabled = true;
     }
 
     public override void OnCreatedRoom()
     {
         Debug.Log("방 생성 성공");
+        StatusServer();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -69,26 +94,105 @@ public class PhotonLauncher : MonoBehaviourPunCallbacks
     {
         Debug.Log("방 입장 성공");
         Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+        photonView.RPC("SyncCurrentRoomPlayer", RpcTarget.Others, true);
+        StatusServer();
+        leaveRoomButton.gameObject.SetActive(true);
+        buttonState.text = "Joined Room";
+        joinRoomButton.enabled = false;
     }
 
-    public override void OnJoinRoomFailed(short returnCode, string message)
+    // 방 입장 실패 시 호출되는 콜백 함수
+    public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log("방 입장 실패");
         Debug.Log(message);
         Debug.Log("Returncode : " + returnCode);
-        PhotonNetwork.JoinRandomOrCreateRoom(null, maxPlayer);
-        //PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayer});
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayer });
+    }
+
+    // 방에서 나갈때 호출되는 콜백함수
+    public override void OnLeftRoom()
+    {
+        StatusServer();
+        leaveRoomButton.gameObject.SetActive(false);
+        joinRoomButton.enabled = true;
     }
 
     // 서버 상태 표시
     public void StatusServer()
     {
         Debug.Log(PhotonNetwork.NetworkClientState);
+        serverState.text = "Server State : " + PhotonNetwork.NetworkClientState.ToString();
+        if (PhotonNetwork.InRoom == true)
+        {
+            playerCount.text = "Player Count : " + PhotonNetwork.CurrentRoom.PlayerCount;
+            // playerCount.text = "Player Count : " + PhotonNetwork.CountOfPlayersInRooms.ToString();
+            roomName.text = "Room Name : " + PhotonNetwork.CurrentRoom.Name.ToString();
+        }
+        else
+        {
+            playerCount.text = "Player Count : ";
+            roomName.text = "Room Name : ";
+        }
     }
 
-    // 서버와 연결이 끊겼을 때 호출되는 함수
+    // 서버와 연결이 끊겼을 때 호출되는 콜백 함수
     public void OnDisconnectedFromServer(DisconnectCause Cause)
     {
         Debug.LogError("서버 연결 끊김");
+    }
+
+    // 오브젝트 초기화
+    private void Init()
+    {
+        StatusServer();
+        joinRoomButton.enabled = false;
+        leaveRoomButton.gameObject.SetActive(false);
+        buttonState.text = "Connecting Server";
+    }
+
+    // JoinRoom Button 클릭시 실행되는 함수
+    public void OnClick_Join_Room()
+    {
+        PhotonNetwork.JoinRandomRoom(null, maxPlayer);
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount >= maxPlayer)
+        {
+            Debug.Log("LeaveRoom");
+            PhotonNetwork.LeaveRoom();
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            Debug.Log("CurrentRoom : " + PhotonNetwork.CurrentRoom.IsOpen);
+            PhotonNetwork.JoinRandomRoom(null, maxPlayer);
+        }
+        StatusServer();
+    }
+
+    // LeaveRoom Button 클릭시 실행되는 함수
+    public void OnClick_Leave_Room()
+    {
+        photonView.RPC("SyncCurrentRoomPlayer", RpcTarget.Others, false);
+        PhotonNetwork.LeaveRoom();   
+    }
+
+    // 현재의 플레이어 수 동기화
+    [PunRPC]
+    public void SyncCurrentRoomPlayer(bool roomState)
+    {
+        if (roomState)
+        {
+            playerCount.text = "Player Count : " + PhotonNetwork.CurrentRoom.PlayerCount.ToString();
+        }
+
+        else
+        {
+            playerCount.text = "Player Count : " + (PhotonNetwork.CurrentRoom.PlayerCount - 1).ToString();
+        }
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == maxPlayer && PhotonNetwork.IsMasterClient == true)
+        {
+            // PhotonNetwork.LoadLevel("이동 할 씬 이름");
+            Debug.Log("씬 넘어간다");
+        }
     }
 }
